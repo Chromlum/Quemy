39d9c1cb1b685ecaf91c5887ce9efada44b9339e
Jose Rodrigo Custodio <jorocuva@gmail.com>
2015-10-14 23:49:14 -0600

    Next Advanced

---
+++ core/src/screens/LevelScreen.java
@@ -0,0 +1,43 @@
+package screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Chromz on 10/14/2015.
+ */
+public class LevelScreen implements Screen {
+    @Override
+    public void dispose() {
+        
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
